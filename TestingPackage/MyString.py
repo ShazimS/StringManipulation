95ba00126d52fa8a8a1a99f15885048d05145f71
Student_12 <shazimsalim@gmail.com>
2016-04-27 15:37:47 -0400

    String

--- TestingPackage/MyString.py
+++ TestingPackage/MyString.py
@@ -24,18 +24,40 @@ class MyString():
 
     #Returns a string that consists of the substring between start and end indexes (both included) in the current string.
     #Index 1 corresponds to the first character in the current string.'''
-    def getSubstring( self,start, end):
+    def getSubstring( self,start,end):
+        c=""
+        try:
+            if start<=0:
+                return("Invalid range")
+            else:
+                for b in range(start-1,end):
+                    c=c+self.str[b]
+                return c
+        except IndexError:
+                return("Invalid range")
 
-        return 1
     #Breaks the string down, and returns it as a character string
     def getCharList(self):
+        mylist=[]
+        count=len(self.str)
 
-        return 1
+        for d in range (0,count):
+            mylist.append(self.str[d])
+
+        return mylist
 
     #Returns the index of the first occurrence of a character in the current string.
     #Index 1 corresponds to the first character in the current string.
     # return 0 if no match is found
     def indexOf(self,c):
+        '''length=len(self.str)
+        start=length-length+1
+            for e in range(start,length):
+                if c=e:
+                    return e=-1 '''
+
+
+
 
         return 0
 
